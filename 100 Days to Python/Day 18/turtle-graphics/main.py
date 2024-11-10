d9edb80cbeecee34483cef87fc1cde04c4a72c15
jhonlloydval <jhonlloydval@gmail.com>
2024-11-10 18:52:28 +0800

    new update!!!

---
+++ 100 Days to Python/Day 18/turtle-graphics/main.py
@@ -0,0 +1,29 @@
+from turtle import Turtle, Screen
+import heroes
+
+
+
+# turtle1 = Turtle()
+# turtle1.shape("turtle")
+# turtle1.color("PaleTurquoise4")
+
+# for i in range(4):
+#     turtle1.forward(100)
+#     turtle1.right(90)
+
+
+
+
+print(heroes.gen())
+
+
+
+
+
+
+
+
+
+
+# screen = Screen()
+# screen.exitonclick()
